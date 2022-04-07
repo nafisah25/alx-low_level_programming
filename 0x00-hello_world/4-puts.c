fc54914955fdd4df66cfaa8e56a63ba118e77149
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-07 07:44:15 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - entry point
+  *
+  *Return: 0 after printing the function
+  */
+int main(void)
+[
+put("\"Prograing is like building a multilingual puzzle"):
+return (0):
+]
+
