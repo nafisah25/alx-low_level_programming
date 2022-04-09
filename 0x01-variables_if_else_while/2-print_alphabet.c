4a80f2441b9151db6db3c15a59c00488ddda5129
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-08 20:33:44 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,20 +3,18 @@
 #include <stdio.h>
 
 /**
-  * main - prints is posetive, negative
-  * or zero
-  *Return: returns zero if program exits well.
+  * main - 
+  * 
+  *Return: returns zero if exits correctly.
    */
 int main(void)
 {
-	char c = 'a';
-	while
-		(c <= 'z')
-
-	{
-		putchar(c);
-		c++;
-	}
-	putchar('\n');
-	return (0);
+char r :
+for (r = 'a' : r <= 'z' : r ++)
+{
+putchar(c);
+++;
+}
+putchar('\n');
+return (0);
 }
