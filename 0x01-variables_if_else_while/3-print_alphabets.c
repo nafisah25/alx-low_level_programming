32affd6fb926d8d9e74ec4b77ac07f30040c0f4e
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-11 06:35:56 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - print positive, negative
+  * or zero
+  * Return: returns 0 if successful.
+  */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
