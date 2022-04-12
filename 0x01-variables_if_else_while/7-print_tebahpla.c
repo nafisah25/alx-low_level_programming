68b8b443f02919e1dd72f5c8a70f1e9c189511dc
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-11 17:40:06 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - prints either positive, negative or zero
+  *
+  * Return: returns 0 if exits successfully.
+  */
+int main (void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
