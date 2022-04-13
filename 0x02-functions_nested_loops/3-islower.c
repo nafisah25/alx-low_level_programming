b214e97c686483181e2c75fb0ef11b1df4c80573
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-13 04:45:02 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _islower - to check for lowercase characters
+  *
+  * Return: Always 0
+  */
+int _islower(int c)
+
+{
+
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
