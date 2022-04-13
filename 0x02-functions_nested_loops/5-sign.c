8f69dc218734433ffe87c2009ff465f7762e9c18
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-13 08:58:11 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,26 +5,21 @@
   * @n: number to check
   * Return: Always 0.
   */
-
 int print_sign(int n)
 {
-	int test;
-
 	if (n > 0)
 	{
-		test = 1;
 		_putchar('+');
+		return (1);
 	}
-	else if (n == 0)
+	else if (n < 00)
 	{
-		test = 0;
-		_putchar(0);
+		_putchar('-');
+		return (-1);
 	}
 	else
 	{
-		test = (-1);
-		_putchar('-');
+		_putchar(0);
+		return (0);
 	}
-
-	return (test);
 }
