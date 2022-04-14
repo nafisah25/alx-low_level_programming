7e95d938ab23e7ee6c4ebc5da76c1738245e5dad
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-14 05:51:33 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
   * largest_number - return the largest bumber of 3
   * @a: first integer
@@ -12,11 +11,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
