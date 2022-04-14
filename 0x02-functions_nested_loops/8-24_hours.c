0a168061a6d3be8e50ddd2432b2f3f3da5c9d589
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-13 20:27:08 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * jack_bauer - function that orints every minutes of the day
+  * from 00:00 - 23:59
+  * Return: 0
+  */
+
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 19;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(mins_remainder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}
+}
+
