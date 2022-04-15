cab552ca685e7ec54bfed78de17cc281f23dcea3
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-15 13:48:56 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_numbers - function that print numbers from
+  * 0 to 9
+  * Return: nothing
+  */
+
+void print_numbers(void)
+{
+	int number = 48;
+	for (number = 48; number < 58; number++)
+	{
+		_putchar(number);
+	}
+	        _putchar(10);
+}
