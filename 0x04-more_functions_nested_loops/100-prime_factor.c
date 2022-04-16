f14a259f01edb8a17687a02f22b2a98fa50b4d7c
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-15 21:19:33 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,16 +8,13 @@
 
 int main(void)
 {
-	long int x = 612852475143;
-	long int py;
+	unsigned long int i = 3, n = 612852475143;
 
-	for (py = 2; py < x; py++)
-		{
-			if (x % py == 0)
-			{
-				x = x / py;
-			}
-		}
-	printf("%ld\n", py);
+	for (; i < 12057; i += 2)
+	{
+		while (n % i == 0 && n != i)
+			n /= i;
+	}
+	printf("%lu\n", n);
 	return (0);
 }
