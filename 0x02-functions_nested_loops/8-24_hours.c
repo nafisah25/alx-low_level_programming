7084361912f2bf7cc817eb38066a9a1317bf4eb4
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-16 22:44:36 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,21 +8,27 @@
 
 void jack_bauer(void)
 {
-	int a;
-	int b;
+	int hours = 0;
+	int minutes = 0;
+	int hours_remainder;
+	int mins_remainder;
 
-	for (a = 0; a <= 23; a++)
+	while (hours <= 23)
 	{
-		for (b = 0; b <= 59; b++)
-			{
-				_putchar (a / 10 + '0');
-				_putchar (a % 10 + '0');
-				_putchar (':');
-				_putchar (b / 10 + '0');
-				_putchar (b % 10 + '0');
-				_putchar ('\n');
-
-			}
+		while (minutes <= 59)
+		{
+			mins_remainder = minutes % 10;
+			hours_remainder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(mins_remainder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
 	}
 
 }
