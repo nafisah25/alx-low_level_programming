24f71aa3567b9773e1a69edbf00f8f9cf2fa1c1d
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-16 23:11:08 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * add - add two integers
+  * @i: print int i
+  * @j: print int j
+  * Return: 0
+  */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
