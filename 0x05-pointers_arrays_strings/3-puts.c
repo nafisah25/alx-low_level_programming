e6e22237dc60c11c4734141d81327873f872addf
Aderemi Nafisat <nafisataderemi1@gmail.com>
2022-04-20 09:38:06 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _puts - a function that prints a string
+  * followed by a new line
+  * @str: string
+  * Return: nothing
+  */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
